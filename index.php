dacec47bbc57d7f759b202969882210d00ff736a
ahmetsengr <98687596+ahmetsengr@users.noreply.github.com>
2022-02-11 22:29:53 +0300

    uzmanınabırak-web
    uzmanınabırak-web

---
+++ index.php
@@ -0,0 +1,374 @@
+<?php
+include 'functions.php';
+
+$services = service_get_all();
+
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+		<meta charset="UTF-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<title>Uzmanı Baksın</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- Favicon Icon -->
+        <link rel="icon"  type="image/png" href="assets/UY ICON/logo/blue.png">		
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css"/>
+        <link rel="stylesheet" type="text/css" href="venobox/venobox.css"/>
+        <link rel="stylesheet" type="text/css" href="assets/css/plugin_theme_css.css"/>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>        
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+		<link rel="stylesheet" type="text/css" href="assets/css/responsive.css"/>
+		<!-- modernizr js -->	
+        <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>		
+		<style>.sag{width: 900px;margin: auto;}
+			   .sol{float: left; padding: 25px 30px 100px 10px;  }
+				.sol1{width: 900px;margin: auto;}
+				.sag1{float: right; padding: 25px 30px 100px 10px;  }
+	</style>
+    </head>
+<body>
+<?php include 'header.php' ?>
+			
+
+	</div>
+</div>		
+<table>
+	<tr>
+		<td style="text-align: center; vertical-align: bottom;"> 
+<div class="sag">
+<img src="assets/UY ICON/uygulama içi bilgilendirme resimleri/ha 21 ikon.png"class="sol">
+<h1>En acil durumda bile</h1>
+<h1>ustaya hemen ulşabileceğin</h1>
+<h1> UZMANA BAK'a HOŞGELDİN</h1>
+</div>
+</td>
+</tr>
+</table>		
+<table>
+<tr>
+		<td style="text-align: center; vertical-align:  bottom;"> 
+<div class="sol1">
+<img src="assets/UY ICON/uygulama içi bilgilendirme resimleri/third_slider_screen_image.png"class="sag1">
+<h1>İşini Anında yaptırabileceğin aradığın</h1>
+<h1>hizmet almak istediğin işi seç,</h1>
+<h1>ne zaman yapılacağını</h1>
+<h1>bildir UZMANI BAKSIN</h1>
+</div>
+</td>
+</tr>
+</table>
+
+<table>
+<tr>
+		<td style="text-align: center; vertical-align:  bottom;"> 
+<div class="sag">
+<img src="assets/UY ICON/uygulama içi bilgilendirme resimleri/second_slider_screen_image.png"class="sol">
+<h1>İşini ehliysen,</h1>
+<h1>hizmet 	verenler arasındaki</h1>
+<h1>yerini hemen al</h1>
+</div></td>
+</tr></table>
+	<!-- solutech_features_area -->
+<div class="solutech_features_area">
+	<div class="container">	
+		<div class="row features_bg">
+			<!-- solutech_features_01 -->
+			<div class="col-lg-4 col-md-6 col-sm-12 pdlf">	
+				<div class="em-service2 sleft all_color_service">
+					<div class="em_service_content ">
+						<div class="em_single_service_text width_height_link_0 ">
+							<div class="service_top_image"></div>
+							<div class="text_box witr_s_flex">
+								<div class="service_top_text all_icon_color">
+									<div class="em-service-icon">
+										<img src="assets/images/feature1.png" alt="">
+									</div>			
+								</div>
+								<div class="em-service-inner">
+									<div class="em-service-title">
+										<h3><a href="#">KORUMA</a></h3>
+									</div>						
+									<div class="em-service-desc">
+										<p>GÜVENLİ SİSTEM</p>		
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- solutech_features_02 -->
+			<div class="col-lg-4 col-md-6 col-sm-12 pdlf">	
+				<div class="em-service2 sleft all_color_service">
+					<div class="em_service_content ">
+						<div class="em_single_service_text width_height_link_0 ">
+							<div class="service_top_image"></div>
+							<div class="text_box witr_s_flex">
+								<div class="service_top_text all_icon_color">
+									<div class="em-service-icon">
+										<img src="assets/images/feature2.png" alt="">
+									</div>			
+								</div>
+								<div class="em-service-inner">
+									<div class="em-service-title">
+										<h3><a href="#">KOLAYLIK</a></h3>
+									</div>						
+									<div class="em-service-desc">
+										<p>BASİT VE ANLAŞILIR ARAYÜZ</p>		
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- solutech_features_03 -->
+			<div class="col-lg-4 col-md-6 col-sm-12 pdlf">	
+				<div class="em-service2 sleft all_color_service border_lf">
+					<div class="em_service_content ">
+						<div class="em_single_service_text width_height_link_0 ">
+							<div class="service_top_image"></div>
+							<div class="text_box witr_s_flex">
+								<div class="service_top_text all_icon_color">
+									<div class="em-service-icon">
+										<img src="assets/images/feature3.png" alt="">
+									</div>			
+								</div>
+								<div class="em-service-inner">
+									<div class="em-service-title">
+										<h3><a href="#">ORGANİZASYON</a></h3>
+									</div>						
+									<div class="em-service-desc">
+										<p>TEKLİFLERİ AL VE EĞLENCEYE DEVAM ET</p>		
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- solutech_about_area -->
+<div class="solutech_about_area">
+	<div class="container">	
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="witr_section_title">
+					<div class="witr_section_title_inner text-center">
+						<h2>ÖNE ÇIKANLAR</h2>		
+						<h3>KULLANICILARIN EN SEVDİĞİ</h3>		
+						<h1>İşler</h1>		
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="witr_pslide3 all_pslides_color ps1 ps3 service_active">
+			<div class="witr_cslide3_idany service_top">
+				<!-- solutech_serivce_01 -->
+				<div class="item_pos col-lg-12">	
+					<div class="witr_single_pslide">	
+						<div class="witr_pslide_image">
+							<img src="assets/images/service-img-1.jpg" alt="">
+						</div>			
+						<div class="witr_content_pslide_text">
+							<div class="witr_number_pslide">
+								<h4>4,8</h4>
+							</div>
+							<div class="witr_content_pslide">
+								<p>TEMPUR SİNEMA</p>		
+								<h3><a href="#">CİNEMAXIMUM</a></h3>
+							</div>
+							<div class="witr_pslide_custom">	
+								<a href="#"><span class="ti-arrow-right"></span></a>
+							</div>
+						</div>	
+					</div> 
+				</div>
+				<!-- solutech_serivce_02 -->
+				<div class="item_pos col-lg-12">	
+					<div class="witr_single_pslide">	
+						<div class="witr_pslide_image">
+							<img src="assets/images/service-img-2.jpg" alt="">
+						</div>			
+						<div class="witr_content_pslide_text">
+							<div class="witr_number_pslide">
+								<h4>4,7</h4>
+							</div>
+							<div class="witr_content_pslide">
+								<p>AÇIK HAVA KONSERLERİ</p>		
+								<h3><a href="#">BİLETİX</a></h3>
+							</div>
+							<div class="witr_pslide_custom">	
+								<a href="#"><span class="ti-arrow-right"></span></a>
+							</div>
+						</div>	
+					</div> 
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- solutech_serivce_bg_area -->
+<div class="solutech_serivce_area serivce_bg">
+	<div class="container">	
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="witr_section_title">
+					<div class="witr_section_title_inner text-center">
+						<h2> BAZI KATEGORİLER</h2>		
+						<h3>İşini Yarım Bırakma</h3>		
+					</div>
+				</div>
+			</div>							
+		</div>
+		<div class="row service_top">
+			<!-- solutech_serivce_bg_01 -->
+			<div class="col-lg-4 col-md-6 col-sm-12">
+				<div class="all_feature_color single_feature2 text-center">            
+					<div class="witr_feature2">					
+						<div class="witr_fea2_icon_top">
+						<i class="fas fa-film"></i>						</div>
+						<h3><a href="#">SİNEMA</a></h3>
+						<p>EŞSİZ FİLM İZLEME FIRSATLARINI KAÇIRMA</p>		
+						<div class="witr_feature_btn2">
+							<a class="witr_btn_all_color" href="#">GÖZ AT</a>
+						</div>
+						<div class="witr_feature2_icon">						
+						<i class="fas fa-film"></i>						</div>					
+					</div> 	   
+				</div>
+			</div>
+			<!-- solutech_serivce_bg_02 -->
+			<div class="col-lg-4 col-md-6 col-sm-12">
+				<div class="all_feature_color single_feature2 text-center">            
+					<div class="witr_feature2">					
+						<div class="witr_fea2_icon_top">
+						<i class="fas fa-theater-masks"></i>						</div>
+						<h3><a href="#">TİYATRO</a></h3>
+						<p>BU SAHNE SANATLARI SİZİ BAŞKA BİR DÜNYAYA GÖTÜRECEK</p>		
+						<div class="witr_feature_btn2">
+							<a class="witr_btn_all_color" href="#">GÖZ AT</a>
+						</div>
+						<div class="witr_feature2_icon">						
+						<i class="fas fa-theater-masks"></i>						</div>					
+					</div> 	   
+				</div>
+			</div>
+			<!-- solutech_serivce_bg_03 -->
+			<div class="col-lg-4 col-md-6 col-sm-12">
+				<div class="all_feature_color single_feature2 text-center">            
+					<div class="witr_feature2">					
+						<div class="witr_fea2_icon_top">
+						<i class="fas fa-birthday-cake"></i>										
+						</div>
+						<h3><a href="#">DOĞUM GÜNÜ</a></h3>
+						<p>ORGANİZASYON PLANLARINIZ VARSA HİZMET VERENLERE ANINDA ULAŞIN</p>		
+						<div class="witr_feature_btn2">
+							<a class="witr_btn_all_color" href="#">GÖZ AT</a>
+						</div>
+						<div class="witr_feature2_icon">						
+						<i class="fas fa-birthday-cake"></i>											
+						</div>					
+					</div> 	   
+				</div>
+			</div>
+			<!-- solutech_serivce_bg_04 -->
+			<div class="col-lg-4 col-md-6 col-sm-12">
+				<div class="all_feature_color single_feature2 text-center">            
+					<div class="witr_feature2">					
+						<div class="witr_fea2_icon_top">
+						<i class="fas fa-hat-cowboy"></i>										
+						</div>
+						<h3><a href="#">DANS</a></h3>
+						<p>ÇILGIN DANS ETKİNLİKLERİNE HAZIR MISINIZ?</p>		
+						<div class="witr_feature_btn2">
+							<a class="witr_btn_all_color" href="#">GÖZ AT</a>
+						</div>
+						<div class="witr_feature2_icon">						
+						<i class="fas fa-hat-cowboy"></i>											
+						</div>					
+					</div> 	   
+				</div>
+			</div>
+			<!-- solutech_serivce_bg_05 -->
+			<div class="col-lg-4 col-md-6 col-sm-12">
+				<div class="all_feature_color single_feature2 text-center">            
+					<div class="witr_feature2">					
+						<div class="witr_fea2_icon_top">
+						<i class="fas fa-guitar"></i>											
+						</div>
+						<h3><a href="#">MÜZİK</a></h3>
+						<p>SENİ SEVDİĞİN SANATÇILARLA BULUŞTURUYORUZ</p>		
+						<div class="witr_feature_btn2">
+							<a class="witr_btn_all_color" href="#">GÖZ AT</a>
+						</div>
+						<div class="witr_feature2_icon">						
+						<i class="fas fa-guitar"></i>											
+						</div>					
+					</div> 	   
+				</div>
+			</div>
+			<!-- solutech_serivce_bg_06 -->
+			<div class="col-lg-4 col-md-6 col-sm-12">
+				<div class="all_feature_color single_feature2 text-center">            
+					<div class="witr_feature2">					
+						<div class="witr_fea2_icon_top">
+						<i class="fas fa-running"></i>											
+						</div>
+						<h3><a href="#">SPORTİF</a></h3>
+						<p>BİRAZ DA EĞLENEREK SPOR YAPMAYA NE DERSİN?</p>		
+						<div class="witr_feature_btn2">
+							<a class="witr_btn_all_color" href="#">GÖZ AT</a>
+						</div>
+						<div class="witr_feature2_icon">						
+						<i class="fas fa-running"></i>											
+						</div>					
+					</div> 	   
+				</div>
+				
+			</div>
+			
+		</div>
+		<div class="witr_button_area button2 " style="text-align: center;">
+					<div class="witr_btn_style mr">
+						<div class="witr_btn_sinner">	
+							<a href="kategoriler.php" class="witr_btn">TÜMÜNÜ GÖRÜNTÜLE</a>
+						</div>						
+					</div>
+				</div> 	
+	</div>
+</div>
+
+<!-- solutech_project_area -->
+
+
+
+	
+	
+<!-- witrfm_footer_area -->
+<?php include 'footer.php' ?>
+
+        <!-- Include All JS -->
+        <script src="assets/js/vendor/jquery-3.5.1.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/isotope.pkgd.min.js"></script>
+        <script src="assets/js/owl.carousel.min.js"></script>
+        <script src="assets/js/jquery.nivo.slider.pack.js"></script>
+        <script src="assets/js/slick.min.js"></script>
+        <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+        <script src="venobox/venobox.min.js"></script>
+        <script src="assets/js/jquery.appear.js"></script>
+        <script src="assets/js/jquery.knob.js"></script>
+        <script src="assets/js/BeerSlider.js"></script>
+        <script src="assets/js/theme-pluginjs.js"></script>
+        <script src="assets/js/jquery.meanmenu.js"></script>
+        <script src="assets/js/ajax-mail.js"></script>		
+        <script src="assets/js/theme.js"></script>
+	
+	</body>
+</html>
